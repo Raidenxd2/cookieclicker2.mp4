b3f702a46c780d0239616e34462ff5a4ea75a648
Raidenxd2 <64755433+Raidenxd2@users.noreply.github.com>
2024-09-20 15:57:39 -0400

    Fix localization

---
+++ Assets/_Project/Scripts/LocalizationFix.cs
@@ -0,0 +1,29 @@
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Localization;
+using UnityEngine.Localization.Settings;
+
+public class LocalizationFix : MonoBehaviour
+{
+    private void Start()
+    {
+        StartAsync().Forget();
+    }
+
+    private async UniTask StartAsync()
+    {
+        Locale prevLocale = LocalizationSettings.SelectedLocale;
+
+        for (int i = 0; i < LocalizationSettings.AvailableLocales.Locales.Count; i++)
+        {
+            if (LocalizationSettings.AvailableLocales.Locales[i].name == "cc2 localization fix (cc2_localization_fix)")
+            {
+                await UniTask.WaitForSeconds(0.1f);
+                LocalizationSettings.SelectedLocale = LocalizationSettings.AvailableLocales.Locales[i];
+            }
+        }
+
+        await UniTask.WaitForSeconds(0.1f);
+        LocalizationSettings.SelectedLocale = prevLocale;
+    }
+}
