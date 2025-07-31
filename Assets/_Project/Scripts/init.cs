7e280197bd5e11caa42913ddb04cb9e2911f8b06
lemres <64755433+Raidenxd2@users.noreply.github.com>
2025-07-30 20:22:59 -0400

    github desktop more like why does this exist

--- Assets/_Project/Scripts/init.cs
+++ Assets/_Project/Scripts/init.cs
@@ -1,7 +1,6 @@
 using Cysharp.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
-using UnityEngine.Rendering.Universal;
 using UnityEngine.SceneManagement;
 
 public class init : MonoBehaviour
