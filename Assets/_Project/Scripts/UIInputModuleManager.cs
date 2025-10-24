ba4421f41f6c07bacbacdaf84d484f4005c95166
Raidenxd2 <64755433+Raidenxd2@users.noreply.github.com>
2025-10-24 13:24:54 -0400

    a

--- Assets/_Project/Scripts/UIInputModuleManager.cs
+++ Assets/_Project/Scripts/UIInputModuleManager.cs
@@ -1,5 +1,9 @@
 using UnityEngine;
+#if UNITY_WEBGL
+using UnityEngine.EventSystems;
+#elif !UNITY_WEBGL
 using UnityEngine.InputSystem.UI;
+#endif
 #if !CC2_REMOVE_VR_SUPPORT
 using UnityEngine.XR.Interaction.Toolkit.UI;
 #endif
@@ -19,10 +23,15 @@ public class UIInputModuleManager : MonoBehaviour
             GetComponent<InputSystemUIInputModule>().enabled = true;
         }
     }
-#else
+#elif !UNITY_WEBGL
     private void Start()
     {
         GetComponent<InputSystemUIInputModule>().enabled = true;
+    }
+#elif UNITY_WEBGL
+    private void Start()
+    {
+        GetComponent<StandaloneInputModule>().enabled = true;
     }
 #endif
 }
