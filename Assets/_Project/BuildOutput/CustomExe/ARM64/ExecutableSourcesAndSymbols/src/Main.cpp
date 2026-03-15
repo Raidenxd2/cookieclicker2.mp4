242fc736692d9d843524cbaa004b09ff91fa6e36
Raidenxd2 <64755433+Raidenxd2@users.noreply.github.com>
2026-03-14 21:33:19 -0400

    Update executables

--- Assets/_Project/BuildOutput/CustomExe/ARM64/ExecutableSourcesAndSymbols/src/Main.cpp
+++ Assets/_Project/BuildOutput/CustomExe/ARM64/ExecutableSourcesAndSymbols/src/Main.cpp
@@ -1,10 +1,9 @@
 #include "PrecompiledHeader.h"
-#include "..\UnityPlayerStub\Exports.h"
+#include "../UnityPlayerStub/Exports.h"
 #include <iostream>
 #include <fstream>
+#include <atlstr.h>
 #include "Registry.hpp"
-
-using namespace std;
 
 using namespace m4x1m1l14n;
 
@@ -15,19 +14,18 @@ extern "C"
 {
     __declspec(dllexport) DWORD NvOptimusEnablement = 0x00000001;
     __declspec(dllexport) int AmdPowerXpressRequestHighPerformance = 1;
-    __declspec(dllexport) extern const UINT D3D12SDKVersion = 611;
+    __declspec(dllexport) extern const UINT D3D12SDKVersion = 618;
     __declspec(dllexport) extern const char* D3D12SDKPath = u8".\\D3D12\\";
 }
 
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, LPWSTR lpCmdLine, int nShowCmd)
 {
     LPCWSTR customDataFolder = nullptr;
-
     LPWSTR newCmd = lpCmdLine;
 
     try
     {
-        // Open the key HKEY_CURRENT_USER\Software\raiden\Cookieclicker2.mp4
+        // Open the key HKEY_CURRENT_USER\Software\OneWing\The Great Bean Shootout
         auto key = Registry::CurrentUser->Open(L"Software\\raiden\\Cookieclicker2.mp4");
 
         // Gets the value of the DWORD GraphicsAPI_h1886732360
@@ -38,7 +36,7 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, LPWSTR lpCmdLine, int nShowC
 
         if (GraphicsAPI == 0)
         {
-
+            printf("AAAA");
         }
         else if (GraphicsAPI == 1)
         {
@@ -61,9 +59,9 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, LPWSTR lpCmdLine, int nShowC
     }
     catch (const std::exception& e)
     {
-        // Create the file CC2PreInitLog.txt where the game is located and log the exception
-        std::ofstream outfile("CC2PreInitLog.txt");
-        outfile << "(wWinMain) Failed to read registry (HKEY_CURRENT_USER\\Software\\raiden\\Cookieclicker2.mp4\\GraphicsAPI_h1186732360) or failed to start the game with an argument\nException info:\n";
+        // Create the file BeanShootoutPreInitLog.txt where the game is located and log the exception
+        std::ofstream outfile("BeanShootoutPreInitLog.txt");
+        outfile << "(wWinMain) Failed to read registry (HKEY_CURRENT_USER\\Software\\raiden\\Cookieclicker2.mp4\\GraphicsAPI_h1886732360) or failed to start the game with an argument\nException info:\n";
         outfile << e.what();
         outfile.close();
     }
